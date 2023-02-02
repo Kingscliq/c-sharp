255113c49b30b9113717fddb0a3326c5065b455e
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-01 21:41:42 -0800

    Catching exceptions in your code

--- HelloKings/Variables.cs
+++ HelloKings/Variables.cs
@@ -6,17 +6,33 @@ namespace HelloKings;
 	{
     static void Main(string[] args)
 	{
-		byte number = 0;
-		string firstName = "Kingsley";
-		bool isPlaying = false;
-		Console.WriteLine(number);
+		//byte number = 0;
+		//string firstName = "Kingsley";
+		//bool isPlaying = false;
+		//Console.WriteLine(number);
 
-		Console.WriteLine("Is " + firstName + " Playing " + "The asnwer is " + isPlaying);
+		//Console.WriteLine("Is " + firstName + " Playing " + "The answer is " + isPlaying);
 
-        string character = "2";
+		//string character = "2";
 
-        int convertedInt = Convert.ToInt32(character);
-        Console.WriteLine(convertedInt);
-    }  
+		//int convertedInt = Convert.ToInt32(character);
+		//Console.WriteLine(convertedInt);
+
+
+		try
+		{
+			string series = "123344";
+			byte b = Convert.ToByte(series);
+			Console.WriteLine(b);
+
+		}
+		catch (Exception)
+		{
+			Console.WriteLine("The string type cannot be converted to an integer");
+
+		}
+		
+
+	}  
 	
 	}
