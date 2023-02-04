56317ddca1a0f49441a1662225d2d82cc4655c09
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-03 22:07:35 -0800

    comparism operators

--- HelloKings/Variables.cs
+++ HelloKings/Variables.cs
@@ -42,6 +42,22 @@ namespace HelloKings;
 
 		Console.WriteLine("Sum is " + sum);
 
+		if(sum == 25)
+		{
+			Console.WriteLine("This is your Silver Jubilee");
+		}
+		else if(sum < 25)
+		{
+			Console.WriteLine("You are not yet in your silver jubilee");
+		}else if(sum > 25 && sum == 50)
+		{
+			Console.WriteLine("This is your Golden Jubilee");
+		}
+		else
+		{
+			Console.WriteLine("None of our values is Matched");
+		}
+
 
 	}
 
