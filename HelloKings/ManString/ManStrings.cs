37f5947986e2db66044053a7913c159b32a636db
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-07 19:54:11 -0800

    ci: using the join keyword for manupulating strings

--- HelloKings/ManString/ManStrings.cs
+++ HelloKings/ManString/ManStrings.cs
@@ -11,11 +11,16 @@ namespace HelloKings.ManString
 			string lastName = "Kingsley";
 			string fullName = string.Format("My fullname is {0} {1}", firstName, lastName);
 
+			string[] names = new string[3] { "John", "Emeka", "Ebuka" };
+
+			var fomattedNames = string.Join(",", names);
+
 			Console.WriteLine(fullName);
+            Console.WriteLine(fomattedNames);
 
 
 
-		}
+        }
 	}
 }
 
