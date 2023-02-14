e52a3c6f85615a471a34cf9fe3233ced1767fcd7
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-13 20:55:32 -0800

    Addrange method

--- HelloKings/ListsArrays/ListMethods.cs
+++ HelloKings/ListsArrays/ListMethods.cs
@@ -8,13 +8,13 @@ namespace HelloKings.ListsArrays
 			var numbers = new List<int>() { 1, 4, 6,7,8};
 
 			numbers.Add(3);
+			numbers.AddRange(new int[]{ 4,6,7,45});
 
 			foreach(int num in numbers)
 			{
 				Console.WriteLine(num);
 			}
 
-			//Console.WriteLine(numbers);
 		}
 	}
 }
