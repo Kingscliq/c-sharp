0cd590c0ebc6b648e87f4a6445bc25facd3d5e77
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-13 23:30:38 -0800

    replace method

--- HelloKings/Strings/StringMethods.cs
+++ HelloKings/Strings/StringMethods.cs
@@ -6,12 +6,15 @@ namespace HelloKings.Strings
 		public static void ManipulateString()
 		{
 			var fullName = "Ajaezo Kingsley ";
-
 			
 			var names = fullName.Split(' ');
             Console.WriteLine("Trimmed: '{0}'", fullName.Trim());
             Console.WriteLine("FirstName: {0}", names[0]);
             Console.WriteLine("LastName: {0}", names[1]);
+
+			var newName = fullName.Replace("Kingsley", "Ezenwa");
+
+            Console.WriteLine("New Name: {0}", newName);
         }
 
 	}
